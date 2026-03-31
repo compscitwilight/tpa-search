cf12a49f3f2e8d06a64caece8223753bd8ea9d03
compscitwilight <compscitwilight@gmail.com>
2026-03-31 04:49:08 -0500

    remove search from layout

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -13,23 +13,6 @@ createRoot(document.getElementById('root')!).render(
       <div className="bg-black text-white py-4 lg:px-16">
         <a className="text-2xl text-nowrap" href="/">The Pony Archive Search</a>
       </div>
-      <form action="/search" className="flex flex-col lg:flex-row items-center gap-2 mt-6 lg:w-1/2 m-auto" method="GET">
-        <div className="flex-1">
-          <select className="border border-gray-400 p-2 cursor-pointer outline-none" name="type">
-            <option>All Types</option>
-            <option value="Image">Images</option>
-            <option value="Video">Videos</option>
-            <option value="Audio">Audio</option>
-            <option value="Text">Text</option>
-            <option value="Compressed">Compressed/Archives</option>
-            <option value="Unknown">Other</option>
-          </select>
-        </div>
-        <div className="flex flex-col lg:flex-row gap-4">
-          <input className="border border-gray-400 p-2 outline-none" placeholder="Query" name="q" type="text" required />
-          <button className="border border-gray-400 p-2 cursor-pointer" type="submit">Search</button>
-        </div>
-      </form>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
