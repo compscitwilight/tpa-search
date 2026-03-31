876d8caa1f88f6fbcd33e4b6c77da76a87b6f3f6
compscitwilight <compscitwilight@gmail.com>
2026-03-31 04:50:34 -0500

    add more supported filters to search, and use trigrams

--- server/index.ts
+++ server/index.ts
@@ -1,32 +1,56 @@
 import express from "express";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
+import type { NodeWhereInput } from "./generated/prisma/models.ts";
 
 import type { SearchQuery } from "./types.ts";
 import { prisma } from "./utils/db.ts";
+import { Prisma } from "./generated/prisma/client.ts";
+import type { Sql } from "@prisma/client/runtime/client";
 
 const PORT = 3000;
 
 const app = express();
 app.use(cors());
-app.use(rateLimit());
+app.use(rateLimit({
+    limit: 100
+}));
 
 app.get("/api/search", async (request: express.Request<{}, {}, {}, SearchQuery>, response: express.Response) => {
     const { query } = request;
-    const { q, page } = query;
-    const results = await prisma.node.findMany({
-        where: {
-            OR: [
-                { displayName: { contains: q, mode: "insensitive"} },
-                { description: { contains: q, mode: "insensitive" } },
-                { tags: { has: q } }
-            ]
-        },
-        take: 50,
-        skip: 50 * (Math.max(1, Number(page) || 0) - 1)
-    })
-    console.log(query.q);
-    response.status(200).send(results);
+    const { q, skip, type, uploader, beforeDate, afterDate } = query;
+
+    const queryFilter = q ? Prisma.sql`"displayName" % ${q}` : "1";
+    const queryOrder = q ? Prisma.sql`ORDER BY similarity("displayName", ${q}) DESC` : Prisma.empty;
+
+    const typeFilter = (type && type !== "All Types") ?
+        Prisma.sql`AND "type" = ${type}` : Prisma.empty;
+
+    const uploaderFilter = uploader ?
+        Prisma.sql`AND "creator" = ${uploader}` : Prisma.empty;
+
+    let beforeDateFilter: Sql = Prisma.empty;
+    let afterDateFilter: Sql = Prisma.empty;
+
+    if (beforeDate) {
+        const epoch = Date.parse(beforeDate);
+        if (epoch) beforeDateFilter = Prisma.sql`AND "ogDate" < to_timestamp(${epoch / 1000})`;
+    }
+
+    if (afterDate) {
+        const epoch = Date.parse(afterDate);
+        if (epoch) afterDateFilter = Prisma.sql`AND "ogDate" > to_timestamp(${epoch / 1000})`;
+    }
+
+    const results = await prisma.$queryRaw`SELECT * FROM "Node"
+    WHERE ${queryFilter} ${typeFilter} ${uploaderFilter}
+    ${beforeDateFilter} ${afterDateFilter}
+    
+    ${queryOrder}
+    LIMIT 50 OFFSET ${skip || 0};`;
+    response.status(200).send({
+        results
+    });
 })
 
 app.listen(PORT, () => {
--- server/types.ts
+++ server/types.ts
@@ -2,6 +2,9 @@ import { NodeType } from "./generated/prisma/enums.ts"
 
 export interface SearchQuery {
     q?: string
-    page?: string
-    type?: NodeType
+    skip?: string
+    type?: NodeType | "All Types"
+    uploader?: string
+    beforeDate?: string
+    afterDate?: string
 }
