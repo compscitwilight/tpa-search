d4cbfaa98b0e9e97c9c2ffb73d82c7ed63202ca5
compscitwilight <compscitwilight@gmail.com>
2026-03-31 04:48:13 -0500

    significant changes to search result

--- client/src/components/SearchResult.tsx
+++ client/src/components/SearchResult.tsx
@@ -1,7 +1,63 @@
-export function SearchResult() {
+import { useState, useEffect } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { SearchResultType } from "./SearchResultType";
+
+export function SearchResult({ data }: {
+    data: {
+        id: string,
+        type: string,
+        displayName: string,
+        absolutePath: string,
+        description: string,
+        creator?: string,
+        tags: Array<string>,
+        thumbnailUrl?: string,
+
+        ogDate: string,
+        ogSize?: number
+    }
+}) {
+
+    const [processedName, setProcessedName] = useState<string>(data.displayName);
+
+    useEffect(() => {
+        try {
+            setProcessedName(n => decodeURI(n));
+        } catch {
+            console.warn(`failed to process ${processedName}`);
+        }
+    }, [])
+
     return (
-        <div>
-            
-        </div>
+        <ErrorBoundary fallback={<p>Something went wrong.</p>}>
+            <div className="flex flex-col lg:flex-row border border-gray-400/50 rounded-md p-2">
+                <div className="grid gap-4 flex-1">
+                    <div>
+                        <div className="flex items-center gap-4">
+                            <h1 className="text-lg font-bold">{processedName}</h1>
+                            <SearchResultType type={data.type} />
+                        </div>
+                        <a className="text-sky-600 underline text-wrap" target="_blank" href={`https://theponyarchive.com${data.absolutePath}`}>
+                            {data.absolutePath}
+                        </a>
+                    </div>
+                    <div>
+                        <div className="flex gap-1 items-center">
+                            <b>Upload date:</b>
+                            <p>{new Date(Date.parse(data.ogDate)).toDateString()}</p>
+                        </div>
+                        <div className="flex gap-1">
+                            <b>Tags:</b>
+                            <p>{data.tags.length > 0 ? data.tags.join(", ") : "none"}</p>
+                        </div>
+                        {data.creator && <div className="flex gap-1">
+                            <b>Creator:</b>
+                            <a className="text-sky-600 underline" href={`/search?uploader=${data.creator}`}>{data.creator}</a>
+                        </div>}
+                    </div>
+                </div>
+                {data.thumbnailUrl && <img className="w-[256px] object-fit order-first lg:order-last" src={data.thumbnailUrl} />}
+            </div>
+        </ErrorBoundary>
     )
 }
